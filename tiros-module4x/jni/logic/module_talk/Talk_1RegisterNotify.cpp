afb7ba8da338cb8f06c529738b34279d7b7151cf
jiayf <jiayf@tiros.com.cn>
2015-03-31 17:22:39 +0800

    添加爱滔客的回调及支持

---
+++ tiros-module4x/jni/logic/module_talk/Talk_1RegisterNotify.cpp
@@ -0,0 +1,34 @@
+/*
+ * Talk_1RegisterNotify.cpp
+ *
+ *  Created on: 2015-3-31
+ *  Author: jiayf
+ */
+ 
+#include "Module_talk.h"
+
+void CallBack_Talk(void* pvUser, int dwParam1, const char *dwParam2){
+	
+	JNIEnv * g_env;
+	jvm->AttachCurrentThread(&g_env,0);
+	
+	dbgprintf("jyf-----JNI-----goluk-----CallBack_Talk--11: %d",dwParam1);
+	
+	dbgprintf("IPCManager-----JNI------CallBack_Talk----11111");
+	
+	jclass cls =g_env->FindClass("cn/com/mobnote/module/talk/TalkNotifyAdapter");
+	jmethodID mId_CallBack = g_env->GetStaticMethodID(cls,"TalkNotifyCallBack","(ILjava/lang/String;)V");
+	jstring data = 0;
+	if(0 != dwParam2){
+		data = (g_env)->NewStringUTF((char *)dwParam2);
+	}
+	
+	dbgprintf("IPCManager-----JNI------CallBack_ipcManager----22222");
+	
+	g_env->CallStaticVoidMethod(cls,mId_CallBack,dwParam1,data);
+	
+	dbgprintf("IPCManager-----JNI------CallBack_ipcManager----33333");
+	
+	// free
+	g_env->DeleteLocalRef(cls);
+}
