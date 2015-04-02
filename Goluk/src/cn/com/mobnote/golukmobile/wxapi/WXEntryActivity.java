84fb773ac1bf7460bc305fc86c5e394d0a743b18
zenghao <zenghao@xiaocheben.com>
2015-04-02 16:32:06 +0800

    1:增加第三方分享模块

--- Goluk/src/cn/com/mobnote/golukmobile/wxapi/WXEntryActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/wxapi/WXEntryActivity.java
@@ -1,8 +1,24 @@
-
 package cn.com.mobnote.golukmobile.wxapi;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
+
+import cn.com.mobnote.golukmobile.R;
+import cn.com.mobnote.umeng.widget.Constants;
+
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.bean.SocializeEntity;
+import com.umeng.socialize.bean.StatusCode;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.controller.listener.SocializeListeners.SnsPostListener;
 import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
 public class WXEntryActivity extends WXCallbackActivity {
-
+	
 }
---
+++ Goluk/src/cn/com/mobnote/umeng/widget/CustomShareUtil.java
@@ -0,0 +1,16 @@
+package cn.com.mobnote.umeng.widget;
+
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+
+public class CustomShareUtil extends Fragment {
+	
+	public void openShareWindow(){
+		 /**
+	     * 调用postShare分享。跳转至分享编辑页，然后再分享。</br> [注意]<li>
+	     * 对于新浪，豆瓣，人人，腾讯微博跳转到分享编辑页，其他平台直接跳转到对应的客户端
+	     */
+	   CustomShareBoard shareBoard = new CustomShareBoard(getActivity());
+	   shareBoard.showAtLocation(getActivity().getWindow().getDecorView(), Gravity.BOTTOM, 0, 0);
+	}
+}
