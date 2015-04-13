46ddadfb5cffb5d5ad566b2d442a831057745286
[xuhw] <[xuhongwei@xiaocheben.com]>
2015-04-13 20:17:39 +0800

    1.添加视频广场jni文件;

---
+++ Common-Goluk/src/cn/com/mobnote/module/videosquare/VideoSuqareManagerFn.java
@@ -0,0 +1,13 @@
+package cn.com.mobnote.module.videosquare;
+
+import cn.com.mobnote.logic.IGolukCommFn;
+
+public interface VideoSuqareManagerFn extends IGolukCommFn{
+	
+	
+	
+	
+	
+	public void VideoSuqare_CallBack(int event, int msg, int param1, Object param2);
+
+}
