8b204bca10e838d6331f78c61a20ed94f104fe91
zenghao <zenghao@xiaocheben.com>
2015-04-14 16:52:34 +0800

    1:增加视频广场播放页面

--- Goluk/src/cn/com/mobnote/golukmobile/videosuqare/VideoCategoryView.java
+++ Goluk/src/cn/com/mobnote/golukmobile/videosuqare/VideoCategoryView.java
@@ -1,25 +1,136 @@
 package cn.com.mobnote.golukmobile.videosuqare;
 
-
 import cn.com.mobnote.golukmobile.R;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 @SuppressLint("InflateParams")
 public class VideoCategoryView {
-	private Context mContext=null;
-	private RelativeLayout mRootLayout=null;
-	
-	public VideoCategoryView(Context context){
-		mContext=context;
-		mRootLayout = (RelativeLayout)LayoutInflater.from(mContext).inflate(R.layout.video_category, null); 
-	}
-	
-	public View getView(){
-		return mRootLayout;
+	private Context mContext = null;
+	private RelativeLayout mRootLayout = null;
+
+	public VideoCategoryView(Context context) {
+		mContext = context;
+		mRootLayout = (RelativeLayout) LayoutInflater.from(mContext).inflate(
+				R.layout.video_category, null);
+		init();
+
 	}
 
+	private void init() {
+
+		// 热门直播按钮
+		RelativeLayout live_categroy = (RelativeLayout) mRootLayout
+				.findViewById(R.id.live_categroy);
+
+		// 曝光台
+		ImageButton category_btn_one = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_one);
+		// 碰瓷达人
+		ImageButton category_btn_two = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_two);
+		// 路上风景
+		ImageButton category_btn_three = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_three);
+		// 随手拍
+		ImageButton category_btn_four = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_four);
+		// 事故大爆料
+		ImageButton category_btn_five = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_five);
+		// 堵车预警
+		ImageButton category_btn_six = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_six);
+		// 惊险十分
+		ImageButton category_btn_seven = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_seven);
+		// 疯狂超车
+		ImageButton category_btn_eight = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_eight);
+		// 感人瞬间
+		ImageButton category_btn_nine = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_nine);
+		// 传递正能量
+		ImageButton category_btn_ten = (ImageButton) mRootLayout
+				.findViewById(R.id.category_btn_ten);
+
+		live_categroy.setOnClickListener(new click());
+		category_btn_one.setOnClickListener(new click());
+		category_btn_two.setOnClickListener(new click());
+		category_btn_three.setOnClickListener(new click());
+		category_btn_four.setOnClickListener(new click());
+		category_btn_five.setOnClickListener(new click());
+		category_btn_six.setOnClickListener(new click());
+		category_btn_seven.setOnClickListener(new click());
+		category_btn_eight.setOnClickListener(new click());
+		category_btn_nine.setOnClickListener(new click());
+		category_btn_ten.setOnClickListener(new click());
+		
+	}
+
+	class click implements View.OnClickListener {
+
+		@Override
+		public void onClick(View view) {
+			// TODO Auto-generated method stub
+			switch (view.getId()) {
+				case R.id.live_categroy:
+					startActivity();
+					break;
+				case R.id.category_btn_one:
+					startActivity();
+					break;
+				case R.id.category_btn_three:
+					startActivity();
+					break;
+				case R.id.category_btn_four:
+					startActivity();
+					break;
+				case R.id.category_btn_five:
+					startActivity();
+					break;
+				case R.id.category_btn_six:
+					startActivity();
+					break;
+				case R.id.category_btn_seven:
+					startActivity();
+					break;
+				case R.id.category_btn_eight:
+					startActivity();
+					break;
+				case R.id.category_btn_nine:
+					startActivity();
+					break;
+				case R.id.category_btn_ten:
+					startActivity();
+					break;
+				case R.id.category_btn_two:
+					startActivity();
+					break;
+				case R.id.back_btn:
+					VideoSquarePlayActivity vpa = (VideoSquarePlayActivity) mContext;
+					vpa.finish();
+					break;
+				default:
+					break;
+			}
+
+		}
+	}
+
+	public void startActivity() {
+		VideoSquareActivity play = (VideoSquareActivity)mContext;
+		Intent intent = new Intent(); 
+        intent.setClass(play,VideoSquarePlayActivity.class);
+        play.startActivity(intent);
+	}
+
+	public View getView() {
+		return mRootLayout;
+	}
 }
--- Goluk/src/cn/com/mobnote/golukmobile/videosuqare/VideoSquareActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/videosuqare/VideoSquareActivity.java
@@ -1,6 +1,5 @@
 package cn.com.mobnote.golukmobile.videosuqare;
 
-
 import cn.com.mobnote.golukmobile.R;
 import android.app.Activity;
 import android.content.res.Resources;
@@ -10,104 +9,111 @@ import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class VideoSquareActivity extends Activity implements OnClickListener{
-	private VideoSquareAdapter mVideoSquareAdapter=null;
-	private ViewPager mViewPager=null;
+public class VideoSquareActivity extends Activity implements OnClickListener {
+	private VideoSquareAdapter mVideoSquareAdapter = null;
+	private ViewPager mViewPager = null;
 	private ImageView hot = null;
 	private ImageView square = null;
-	
 	private Button mVideoList = null;
 	private Button mTypeList = null;
 	/** 返回按钮 */
 	private Button mBackBtn = null;
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.video_square_main);
-		
-		mViewPager = (ViewPager)findViewById(R.id.mViewpager);
+
+		mViewPager = (ViewPager) findViewById(R.id.mViewpager);
 		mVideoSquareAdapter = new VideoSquareAdapter(this);
 		mViewPager.setAdapter(mVideoSquareAdapter);
 		mViewPager.setOnPageChangeListener(opcl);
 		init();
 		setListener();
 	}
-	
-	private void init(){
+
+	private void init() {
 		hot = (ImageView) findViewById(R.id.line_hot);
 		square = (ImageView) findViewById(R.id.line_square);
 		mVideoList = (Button) findViewById(R.id.mVideoList);
 		mTypeList = (Button) findViewById(R.id.mTypeList);
-		//获取页面元素
-		mBackBtn = (Button)findViewById(R.id.back_btn);
+		// 获取页面元素
+		mBackBtn = (Button) findViewById(R.id.back_btn);
+		
+		
 	}
-	
-	private void setListener(){
+
+	private void setListener() {
 		mVideoList.setOnClickListener(this);
 		mTypeList.setOnClickListener(this);
 		mBackBtn.setOnClickListener(this);
-	}
-	
-	private OnPageChangeListener opcl = new OnPageChangeListener() {
 		
+	}
+
+	private OnPageChangeListener opcl = new OnPageChangeListener() {
+
 		@Override
 		public void onPageSelected(int arg0) {
 			// TODO Auto-generated method stub
 			updateState(arg0);
 		}
-		
+
 		@Override
 		public void onPageScrolled(int arg0, float arg1, int arg2) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 		@Override
 		public void onPageScrollStateChanged(int arg0) {
 			// TODO Auto-generated method stub
-			
+
 		}
 	};
-	
-	private void updateState(int type){
-		
-		if(0 == type){
+
+	private void updateState(int type) {
+
+		if (0 == type) {
 			hot.setVisibility(View.VISIBLE);
 			square.setVisibility(View.INVISIBLE);
-			
-			mVideoList.setTextColor(getResources().getColor(R.color.textcolor_select));
-			mTypeList.setTextColor(getResources().getColor(R.color.textcolor_qx));
-		}else{
+
+			mVideoList.setTextColor(getResources().getColor(
+					R.color.textcolor_select));
+			mTypeList.setTextColor(getResources()
+					.getColor(R.color.textcolor_qx));
+		} else {
 			hot.setVisibility(View.INVISIBLE);
 			square.setVisibility(View.VISIBLE);
-			
-			mVideoList.setTextColor(getResources().getColor(R.color.textcolor_qx));
-			mTypeList.setTextColor(getResources().getColor(R.color.textcolor_select));
+
+			mVideoList.setTextColor(getResources().getColor(
+					R.color.textcolor_qx));
+			mTypeList.setTextColor(getResources().getColor(
+					R.color.textcolor_select));
 		}
 	}
 
 	@Override
 	public void onClick(View arg0) {
 		switch (arg0.getId()) {
-			case R.id.mVideoList:
-				mViewPager.setCurrentItem(0);
-				this.updateState(0);
-				break;
-			case R.id.mTypeList:
-				mViewPager.setCurrentItem(1);
-				this.updateState(1);
-				break;
-			case R.id.back_btn:
-				//返回
-				this.finish();
-				break;
-			default:
-				break;
+		case R.id.mVideoList:
+			mViewPager.setCurrentItem(0);
+			this.updateState(0);
+			break;
+		case R.id.mTypeList:
+			mViewPager.setCurrentItem(1);
+			this.updateState(1);
+			break;
+		case R.id.back_btn:
+			// 返回
+			this.finish();
+			break;
+		default:
+			break;
 		}
 	}
-	
+
 }
