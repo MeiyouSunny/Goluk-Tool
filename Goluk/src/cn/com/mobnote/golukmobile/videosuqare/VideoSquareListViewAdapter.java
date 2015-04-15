0e2d109f696327e6c056ec7d05c9b1260269922b
zenghao <zenghao@xiaocheben.com>
2015-04-15 14:59:11 +0800

    1:时间格式化

--- Goluk/src/cn/com/mobnote/golukmobile/videosuqare/VideoSquareListViewAdapter.java
+++ Goluk/src/cn/com/mobnote/golukmobile/videosuqare/VideoSquareListViewAdapter.java
@@ -1,11 +1,17 @@
 package cn.com.mobnote.golukmobile.videosuqare;
 
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.SimpleFormatter;
 
 import cn.com.mobnote.golukmobile.R;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.provider.ContactsContract.Contacts.Data;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,20 +60,29 @@ public class VideoSquareListViewAdapter extends BaseAdapter {
 			convertView = LayoutInflater.from(mContext).inflate(
 					R.layout.video_square_list_item, null);
 			holder = new ViewHolder();
-			holder.username = (TextView) convertView.findViewById(R.id.looknumber_text);
-			holder.looknumber = (TextView) convertView.findViewById(R.id.username);
-			holder.userhead = (ImageView) convertView.findViewById(R.id.user_head);
-			holder.likenumber = (Button) convertView.findViewById(R.id.like_btn);
-			holder.videotitle = (TextView) convertView.findViewById(R.id.video_title);
+			holder.username = (TextView) convertView
+					.findViewById(R.id.looknumber_text);
+			holder.looknumber = (TextView) convertView
+					.findViewById(R.id.username);
+			holder.userhead = (ImageView) convertView
+					.findViewById(R.id.user_head);
+			holder.likenumber = (Button) convertView
+					.findViewById(R.id.like_btn);
+			holder.videotitle = (TextView) convertView
+					.findViewById(R.id.video_title);
+			holder.sharetime = (TextView) convertView.findViewById(R.id.time);
+
 			convertView.setTag(holder);
-        }else{
-            holder = (ViewHolder)convertView.getTag();
-        }
-		
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+
 		holder.username.setText(mVideoSquareInfo.mUserEntity.nickname);
 		holder.looknumber.setText(mVideoSquareInfo.mVideoEntity.clicknumber);
 		holder.likenumber.setText(mVideoSquareInfo.mVideoEntity.praisenumber);
 		holder.videotitle.setText(mVideoSquareInfo.mVideoEntity.describe);
+		holder.sharetime.setText(this
+				.formatTime(mVideoSquareInfo.mVideoEntity.sharingtime));
 		return convertView;
 	}
 
@@ -80,12 +95,21 @@ public class VideoSquareListViewAdapter extends BaseAdapter {
 
 	}
 
+	@SuppressLint("SimpleDateFormat")
+	public String formatTime(String date) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yy年MM月dd日 HH时mm分");
+		ParsePosition pos = new ParsePosition(0);
+		Date strtodate = formatter.parse(date, pos);
+		return strtodate;
+	}
+
 	public static class ViewHolder {
 		TextView username;
 		TextView looknumber;
 		ImageView userhead;
 		Button likenumber;
 		TextView videotitle;
+		TextView sharetime;
 	}
 
 }
