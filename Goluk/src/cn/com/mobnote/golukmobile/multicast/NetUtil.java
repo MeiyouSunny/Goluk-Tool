1d64e7fbf1f152a50005c6c447dd8ad32065d676
jiayf <jiayufeng@xiaocheben.com>
2015-05-04 14:23:15 +0800

    修改goluk连接方法

--- Goluk/src/cn/com/mobnote/golukmobile/multicast/NetUtil.java
+++ Goluk/src/cn/com/mobnote/golukmobile/multicast/NetUtil.java
@@ -27,6 +27,11 @@ public class NetUtil {
 
 	private static NetUtil mInstance = new NetUtil();
 
+	private IMultiCastFn mFn = null;
+
+	private static final int MSG_H_SUCESS = 1;
+	private static final int MSG_H_ERROR = 2;
+
 	public static NetUtil getInstance() {
 		return mInstance;
 	}
@@ -34,38 +39,23 @@ public class NetUtil {
 	private Handler mHandler = new Handler() {
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
-			case 0:
-				break;
-			case 1:
-
-				break;
-			case 2:
-				break;
-			case 3:
-
-				break;
 			case 4:
-				Log.e("", "SUccess!!!!!!!");
 				isCanScan = false;
 				WifiRsBean bean = (WifiRsBean) msg.obj;
 				sendData(SUCESS, bean);
+				// 连接成功后，关闭连接
+				cancel();
 				break;
 			case 5:
 				break;
 			case 6:
 				Log.e("", "Error!!!!!!!");
+				cancel();
 				break;
-			case 7:
-				break;
-			case 8:
-				break;
-
 			}
 			super.handleMessage(msg);
 		};
 	};
-
-	private IMultiCastFn mFn = null;
 
 	public void setMultiCastListener(IMultiCastFn fn) {
 		mFn = fn;
@@ -92,18 +82,18 @@ public class NetUtil {
 
 	public void findServerIpAddress(int type, final String mac, final String ip, int seconds) {
 
+		if (null != mUdpSocket) {
+			return;
+		}
 		mType = type;
-
 		Log.e("", "++++++++++++++++++++TestUDP--------findServerIpAddress-----1");
-		if (mUdpSocket == null) {
-			try {
-				mUdpSocket = new DatagramSocket(GRAM_PORT);
-				Log.e("", "++++++++++++++++++++TestUDP--------findServerIpAddress-----2");
-			} catch (SocketException e) {
-				mHandler.sendEmptyMessage(6);
-				Log.e("", "++++++++++++++++++++TestUDP--------findServerIpAddress-----3");
-				return;
-			}
+		try {
+			mUdpSocket = new DatagramSocket(GRAM_PORT);
+			Log.e("", "++++++++++++++++++++TestUDP--------findServerIpAddress-----2");
+		} catch (SocketException e) {
+			mHandler.sendEmptyMessage(6);
+			Log.e("", "++++++++++++++++++++TestUDP--------findServerIpAddress-----3");
+			return;
 		}
 
 		isCanScan = true;
@@ -117,43 +107,44 @@ public class NetUtil {
 						mPacket = new DatagramPacket(recvbuf, 256);
 						Log.e("", "++TestUDP--------findServerIpAddress-----5");
 						mUdpSocket.receive(mPacket);
+						Log.e("", "++TestUDP--------findServerIpAddress----6");
 						int length = mPacket.getLength();
 						byte[] data = mPacket.getData();
 						String s = new String(data, 0, length - 1, "GBK");
 						if (s.equals("Goluk,good luck!")) {
-							String address = mPacket.getSocketAddress().toString();
 							String address2 = mPacket.getAddress().toString();
-
-							if (address2.contains("/")) {
-								address2 = address2.replace("/", "");
-							}
-
-							WifiRsBean bean = new WifiRsBean();
-							bean.setIpc_ip(address2);
-
-							Message msg = new Message();
-							msg.what = 4;
-							msg.obj = bean;
-							mHandler.sendMessage(msg);
-
-							Log.e("", "++TestUDP--------findServerIpAddress-----6 recvbuf=Goluk,good luck:  " + s
-									+ " address:" + address + "	address2:" + address2 + "	address3:");
+							receiveSucess(address2);
 							break;
 						} else {
 							Log.e("", "+++TestUDP--------findServerIpAddress-----77777 recvbuf1=" + s);
 						}
 					} catch (Exception e) {
-						Log.e("", "++TestUDP--------findServerIpAddress-------8-898989-ip=");
+						Log.e("", "++TestUDP--------findServerIpAddress-------8888888888-ip=");
 						e.printStackTrace();
 						mHandler.sendEmptyMessage(6);
 						isCanScan = false;
 						return;
 					}
-
-					Log.e("", "++TestUDP--------findServerIpAddress-------8--ip=" + mPacket.getAddress());
+					Log.e("", "++TestUDP--------findServerIpAddress-------99999999999--ip=" + mPacket.getAddress());
 				}
 			}
 		}).start();
+
+	}
+
+	private void receiveSucess(String ip) {
+		Log.e("", "+++TestUDP--------receiveSucess-----1111111 recvbuf1=: " + ip);
+		if (ip.contains("/")) {
+			ip = ip.replace("/", "");
+		}
+
+		WifiRsBean bean = new WifiRsBean();
+		bean.setIpc_ip(ip);
+
+		Message msg = new Message();
+		msg.what = 4;
+		msg.obj = bean;
+		mHandler.sendMessage(msg);
 
 	}
 
