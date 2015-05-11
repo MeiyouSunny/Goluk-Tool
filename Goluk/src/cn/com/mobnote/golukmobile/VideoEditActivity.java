1c4622f3c71f56542f8c9cf225bff2505f5881ca
chenxy <chenxuanyu@xiaocheben.com>
2015-05-11 14:23:43 +0800

    1.本地视频编辑快速返回多添加控制针处理

--- Goluk/src/cn/com/mobnote/golukmobile/VideoEditActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/VideoEditActivity.java
@@ -555,16 +555,18 @@ public class VideoEditActivity extends Activity implements OnClickListener {
 
 	private void exit() {
 		isExit = true;
-		// 判断是否正在上传
-		int t = mVideoLoadingLayout.getVisibility();
-		if (t == 0) {
-			// 正在上传
-			mVideoLoadingLayout.setVisibility(View.GONE);
-			
-			//为了修复上传的是时返回几率崩溃控制针问题.
-			//感觉可能崩溃到这里了,chenxy 5.11
-			if (null != mVVPlayVideo) {
-				mVVPlayVideo.cancelSave();
+		if(null != mVideoLoadingLayout){
+			// 判断是否正在上传
+			int t = mVideoLoadingLayout.getVisibility();
+			if (t == 0) {
+				// 正在上传
+				mVideoLoadingLayout.setVisibility(View.GONE);
+				
+				//为了修复上传的是时返回几率崩溃控制针问题.
+				//感觉可能崩溃到这里了,chenxy 5.11
+				if (null != mVVPlayVideo) {
+					mVVPlayVideo.cancelSave();
+				}
 			}
 		}
 		if (null != mVVPlayVideo) {
