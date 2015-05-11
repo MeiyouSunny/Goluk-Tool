bda2306d607939fb92bd6360ed5eafb5cc369325
[xuhw] <[xuhongwei@xiaocheben.com]>
2015-05-11 15:01:36 +0800

    更新锐动SDK(20150508)

--- Goluk/src/cn/com/mobnote/golukmobile/carrecorder/util/BitmapManager.java
+++ Goluk/src/cn/com/mobnote/golukmobile/carrecorder/util/BitmapManager.java
@@ -19,12 +19,18 @@ public class BitmapManager {
 	}
 	
 	public BitmapManager(){
-		String imagecache = Environment.getExternalStorageDirectory()
+		long max = Runtime.getRuntime().maxMemory();
+		int memoryCacheSize = (int)(max/8);
+		int diskCacheSize = 50*1024*1024;
+		String diskCachePath = Environment.getExternalStorageDirectory()
 				+ File.separator + "tiros-com-cn-ext" + File.separator + "image_cache";
-		mBitmapUtils = new BitmapUtils(GolukApplication.getInstance(), imagecache);
+		mBitmapUtils = new BitmapUtils(GolukApplication.getInstance(), diskCachePath, memoryCacheSize, diskCacheSize);
 		mBitmapUtils.configDefaultBitmapConfig(Bitmap.Config.RGB_565);
+		mBitmapUtils.configMemoryCacheEnabled(true);
+		mBitmapUtils.configThreadPoolSize(3);
+		int screenwidth = SoundUtils.getInstance().getDisplayMetrics().widthPixels;
+		int screenheight = SoundUtils.getInstance().getDisplayMetrics().heightPixels;
+		mBitmapUtils.configDefaultBitmapMaxSize(screenwidth, screenheight);
 	}
 	
-	
-
 }
