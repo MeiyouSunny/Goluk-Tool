2b44354d3d2c4a1017156a35c04a7eba69f138da
lily <liliyue@xiaocheben.com>
2015-05-15 19:36:31 +0800

    解除绑定测试

--- Goluk/src/cn/com/mobnote/golukmobile/UnbindActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/UnbindActivity.java
@@ -2,9 +2,18 @@ package cn.com.mobnote.golukmobile;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class UnbindActivity extends BaseActivity {
 
+	//title
+	private Button mBackBtn = null;
+	private TextView mTextTitle = null;
+	//body
+	private RelativeLayout mHaveipcLayout = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
