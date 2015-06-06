2d8039c1e1d584742d56129f221fc1ea45e24cc6
[xuhw] <[xuhongwei@xiaocheben.com]>
2015-06-06 13:51:13 +0800

    1.恢复地图定位功能;

--- Goluk/src/cn/com/mobnote/golukmobile/MainActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/MainActivity.java
@@ -970,9 +970,9 @@ public class MainActivity extends BaseActivity implements OnClickListener, WifiC
 		switch (id) {
 		case R.id.map_location_btn:
 			// 回到我的位置
-//			LatLng ll = new LatLng(LngLat.lat, LngLat.lng);
-//			MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
-//			mBaiduMap.animateMapStatus(u);
+			LatLng ll = new LatLng(LngLat.lat, LngLat.lng);
+			MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+			mBaiduMap.animateMapStatus(u);
 			
 //			test();
 			break;
