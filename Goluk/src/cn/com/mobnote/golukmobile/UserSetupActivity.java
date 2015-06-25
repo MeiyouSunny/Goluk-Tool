6617c74e1d51d34aa2f996a929b5ac793c900491
lily <liliyue@xiaocheben.com>
2015-06-25 10:44:19 +0800

    Revert "ipc升级"
    This reverts commit 66628fb5c336bcf763ffa6c73a9f4b87e1cc7a5d.

--- Goluk/src/cn/com/mobnote/golukmobile/UserSetupActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/UserSetupActivity.java
@@ -392,13 +392,19 @@ public class UserSetupActivity extends CarRecordBaseActivity implements OnClickL
 				mApp.flag=true;
 //				mApp.mUpgrade.upgradeGoluk();
 				GolukDebugUtils.i("lily", vIpc+"========UserSetupActivity==点击版本检测===中ipcVersion=====");
-				mApp.mIpcUpdateManage.requestInfo(IpcUpdateManage.FUNCTION_SETTING_APP, vIpc);
+				boolean appB = mApp.mIpcUpdateManage.requestInfo(IpcUpdateManage.FUNCTION_SETTING_APP, vIpc);
+				if (appB) {
+					this.showAppLoadingDialog();
+				}
 				break;
 				//固件升级
 			case R.id.update_item:
 //				mApp.mIpcUpdateManage.ipcUpgrade();
 				GolukDebugUtils.i("lily", vIpc+"========UserSetupActivity===点击固件升级==中ipcVersion=====");
-				mApp.mIpcUpdateManage.requestInfo(IpcUpdateManage.FUNCTION_SETTING_IPC, vIpc);
+				boolean b = mApp.mIpcUpdateManage.requestInfo(IpcUpdateManage.FUNCTION_SETTING_IPC, vIpc);
+				if (b) {
+					this.showIpcLoadingDialog();
+				}
 				break;
 		}
 	}
@@ -566,6 +572,48 @@ public class UserSetupActivity extends CarRecordBaseActivity implements OnClickL
 		}
 	} 
     
+    
+    private void showAppLoadingDialog() {
+    	
+    }
+    
+    private void dismissAppLoadingDialog() {
+    	
+    }
+    
+    private void showIpcLoadingDialog() {
+    	
+    }
+    
+    private void dismissIpcLoadingDialog() {
+    	
+    }
+ 
+    
+	/**
+	 * App升级与IPC升级回调方法
+	 * 
+	 * @param function
+	 *            App升级/IPC升级 2/ 3
+	 * @param data
+	 *            交互数据
+	 * @author jyf
+	 * @date 2015年6月24日
+	 */
+	public void updateCallBack(int function, Object data) {
+		switch(function) {
+		case IpcUpdateManage.FUNCTION_SETTING_APP:
+			dismissAppLoadingDialog();
+			// show dialog
+			break;
+		case IpcUpdateManage.FUNCTION_SETTING_IPC:
+			dismissIpcLoadingDialog();
+			break;
+		default:
+				break;
+		}
+	}
+    
     @Override
 	protected void onDestroy() {
 		super.onDestroy();
@@ -597,5 +645,6 @@ public class UserSetupActivity extends CarRecordBaseActivity implements OnClickL
 			mTimer = null;
 		}
 	}
+	
 
 }
