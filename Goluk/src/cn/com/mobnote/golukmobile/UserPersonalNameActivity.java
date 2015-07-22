d800128d8e51cd20b0a87fe7f3361dc50bc67ef6
lily <liliyue@xiaocheben.com>
2015-07-22 15:38:10 +0800

    更改昵称和更改个性签名页光标定位在内容的末尾

--- Goluk/src/cn/com/mobnote/golukmobile/UserPersonalNameActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/UserPersonalNameActivity.java
@@ -16,58 +16,61 @@ import android.widget.TextView;
 
 /**
  * 编辑昵称
+ * 
  * @author mobnote
  *
  */
-public class UserPersonalNameActivity extends BaseActivity implements OnClickListener{
+public class UserPersonalNameActivity extends BaseActivity implements OnClickListener {
 
-	/**title**/
+	/** title **/
 	private ImageButton btnBack;
-	private TextView mTextTitle,mTextOk;
-	/**body**/
+	private TextView mTextTitle, mTextOk;
+	/** body **/
 	private EditText mEditName;
 	private ImageView mImageNameRight;
 	private String nameText;
-	/**文字字数提示**/
+	/** 文字字数提示 **/
 	private TextView mTextCount = null;
-	/**最大输入字数**/
+	/** 最大输入字数 **/
 	private static final int MAX_COUNT = 10;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.user_personal_edit_name);
-		
+
 		initView();
-		//title
+		// title
 		mTextTitle.setText("编辑昵称");
 		mTextOk.setText("确认");
 		//
 		int count = mEditName.getText().toString().length();
-		mTextCount.setText("（"+(MAX_COUNT-count)+"/"+MAX_COUNT+"）");
-		
+		mTextCount.setText("（" + (MAX_COUNT - count) + "/" + MAX_COUNT + "）");
+
 	}
-	//初始化控件
-	public void initView(){
+
+	// 初始化控件
+	public void initView() {
 		btnBack = (ImageButton) findViewById(R.id.back_btn);
 		mTextOk = (TextView) findViewById(R.id.user_title_right);
 		mTextTitle = (TextView) findViewById(R.id.user_title_text);
 		mEditName = (EditText) findViewById(R.id.user_personal_name_edit);
 		mImageNameRight = (ImageView) findViewById(R.id.user_personal_name_image);
 		mTextCount = (TextView) findViewById(R.id.number_count);
-		
+
 		/**
 		 * 获取从编辑界面传来的姓名
 		 */
 		Intent it = getIntent();
-		if(null!=it.getStringExtra("intentNameText")){
+		if (null != it.getStringExtra("intentNameText")) {
 			Bundle bundle = it.getExtras();
 			nameText = bundle.getString("intentNameText");
-			GolukDebugUtils.i("lily", "----------UserPersonalNameActivity---------name====="+nameText);
+			GolukDebugUtils.i("lily", "----------UserPersonalNameActivity---------name=====" + nameText);
 		}
 		mEditName.setText(nameText);
-		
+		mEditName.setSelection(nameText.length());
+
 		/**
 		 * 监听
 		 */
@@ -76,22 +79,23 @@ public class UserPersonalNameActivity extends BaseActivity implements OnClickLis
 		mImageNameRight.setOnClickListener(this);
 		mEditName.addTextChangedListener(mTextWatcher);
 	}
+
 	@Override
 	public void onClick(View arg0) {
 		switch (arg0.getId()) {
-		//返回
+		// 返回
 		case R.id.back_btn:
 			finish();
 			break;
-		//que认
+		// que认
 		case R.id.user_title_right:
 			String name = mEditName.getText().toString();
-			GolukDebugUtils.i("lily", "------UserPersonalNameActivity--修改昵称------"+name);
-			if(name.trim().isEmpty()){
+			GolukDebugUtils.i("lily", "------UserPersonalNameActivity--修改昵称------" + name);
+			if (name.trim().isEmpty()) {
 				UserUtils.showDialog(this, "数据修改失败，昵称不能为空");
-			}else{
+			} else {
 				UserPersonalInfoActivity.clickBtn = true;
-				Intent it = new Intent(UserPersonalNameActivity.this,UserPersonalInfoActivity.class);
+				Intent it = new Intent(UserPersonalNameActivity.this, UserPersonalInfoActivity.class);
 				Bundle bundle = new Bundle();
 				bundle.putString("itName", name);
 				it.putExtras(bundle);
@@ -101,7 +105,7 @@ public class UserPersonalNameActivity extends BaseActivity implements OnClickLis
 			break;
 		//
 		case R.id.user_personal_name_image:
-			//点击清空
+			// 点击清空
 			mEditName.setText("");
 			break;
 
@@ -109,22 +113,25 @@ public class UserPersonalNameActivity extends BaseActivity implements OnClickLis
 			break;
 		}
 	}
+
 	TextWatcher mTextWatcher = new TextWatcher() {
-		
+
 		@Override
 		public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
 		}
+
 		@Override
 		public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
 		}
+
 		@Override
 		public void afterTextChanged(Editable arg0) {
 			int num = arg0.length();
 			int number = MAX_COUNT - num;
-			if(number < 0){
+			if (number < 0) {
 				number = 0;
 			}
-			mTextCount.setText("（"+number + "/"+MAX_COUNT+"）");
+			mTextCount.setText("（" + number + "/" + MAX_COUNT + "）");
 		}
 	};
 
