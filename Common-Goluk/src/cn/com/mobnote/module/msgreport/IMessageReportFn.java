587eccd73cb2f14ec6b0e669ae15e2d0c21fe463
jiayf <jiayufeng@xiaocheben.com>
2015-08-16 11:40:27 +0800

    添加日志上报的接口

---
+++ Common-Goluk/src/cn/com/mobnote/module/msgreport/IMessageReportFn.java
@@ -0,0 +1,14 @@
+package cn.com.mobnote.module.msgreport;
+
+public interface IMessageReportFn {
+
+	/** 网络改变通知，需要平台在应用起来时通知当前网络状态，并且在发生改变时再次通知 */
+	public static final int REPORT_CMD_NET_STATA_CHG = 0;
+	/** 非实时上传，使用http，可靠上传 */
+	public static final int REPORT_CMD_LOG_REPORT_HTTP = 1;
+	/** 实时上传，使用UDP，非可靠上传 */
+	public static final int REPORT_CMD_LOG_REPORT_REAL = 2;
+	
+	
+	public static final String KEY_WIFI_BIND="key_wifi_bind";
+}
