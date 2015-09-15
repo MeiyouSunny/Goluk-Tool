26388ae85187ed28fd6d5147cc2ea26942ee77db
zenghao <zenghao@xiaocheben.com>
2015-09-15 15:01:56 +0800

    1:jjlslkdflj`

---
+++ Goluk/src/cn/com/mobnote/golukmobile/usercenter/UserCenterActivity.java
@@ -0,0 +1,24 @@
+package cn.com.mobnote.golukmobile.usercenter;
+
+import cn.com.mobnote.golukmobile.BaseActivity;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+/**
+ * 
+ * @author 曾浩
+ *
+ */
+@SuppressLint("NewApi")
+public class UserCenterActivity extends BaseActivity {
+	
+
+	@SuppressLint("HandlerLeak")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+
+	}
+
+}
