f3e70b65093d98f5d4997b1cc4463362140a063e
zenghao <zenghao@xiaocheben.com>
2015-09-22 17:52:31 +0800

    1:修改视频列表评论不显示的问题

--- Goluk/src/cn/com/mobnote/golukmobile/usercenter/UserCenterAdapter.java
+++ Goluk/src/cn/com/mobnote/golukmobile/usercenter/UserCenterAdapter.java
@@ -430,7 +430,7 @@ public class UserCenterAdapter extends BaseAdapter implements VideoSuqareManager
 			if (clusterInfo.mVideoEntity.commentList.size() >= 2) {
 				CommentDataInfo comment = clusterInfo.mVideoEntity.commentList.get(1);
 				holder.comment2.setText(comment.name + "  " + comment.text);
-				holder.comment1.setVisibility(View.VISIBLE);
+				holder.comment2.setVisibility(View.VISIBLE);
 			} else {
 				holder.comment2.setVisibility(View.GONE);
 			}
@@ -438,7 +438,7 @@ public class UserCenterAdapter extends BaseAdapter implements VideoSuqareManager
 			if (clusterInfo.mVideoEntity.commentList.size() >= 3) {
 				CommentDataInfo comment = clusterInfo.mVideoEntity.commentList.get(2);
 				holder.comment3.setText(comment.name + "  " + comment.text);
-				holder.comment1.setVisibility(View.VISIBLE);
+				holder.comment3.setVisibility(View.VISIBLE);
 			} else {
 				holder.comment3.setVisibility(View.GONE);
 			}
