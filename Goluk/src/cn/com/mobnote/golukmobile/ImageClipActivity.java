697e2c417ec4ff4713bc72fabb98992da1294bec
zenghao <zenghao@xiaocheben.com>
2015-10-21 15:03:15 +0800

    1:修改图片加载内存溢出问题

--- Goluk/src/cn/com/mobnote/golukmobile/ImageClipActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/ImageClipActivity.java
@@ -58,21 +58,23 @@ public class ImageClipActivity extends BaseActivity implements OnClickListener, 
 		setContentView(R.layout.roadbook_crop_pic);
 		Uri uri = Uri.parse(getIntent().getStringExtra("imageuri"));
 
+		saveHead = (Button) findViewById(R.id.saveBtn);
+		cancelBtn = (Button) findViewById(R.id.cancelBtn);
+		imageView = (ClipImageView) findViewById(R.id.src_pic);
+		
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 4;// 图片宽高都为原来的二分之一，即图片为原来的四分之一
+		Bitmap bitmap;
 		try {
-			Bitmap bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-			saveHead = (Button) findViewById(R.id.saveBtn);
-			cancelBtn = (Button) findViewById(R.id.cancelBtn);
-			imageView = (ClipImageView) findViewById(R.id.src_pic);
-			imageView.setImageBitmap(bmp);
-			siv = new SettingImageView(this);
-			initListener();
+			bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, options);
+			imageView.setImageBitmap(bitmap);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+		
+		siv = new SettingImageView(this);
+		initListener();
 
 	}
 
@@ -159,9 +161,9 @@ public class ImageClipActivity extends BaseActivity implements OnClickListener, 
 
 			f.createNewFile();
 			out = new FileOutputStream(f);
-			
+
 			Bitmap cacheBitmap = this.compress(bm);
-			
+
 			cacheBitmap.compress(Bitmap.CompressFormat.PNG, 80, out);
 
 			requestStr = new JSONObject();
@@ -185,61 +187,57 @@ public class ImageClipActivity extends BaseActivity implements OnClickListener, 
 
 	/**
 	 * 把图片压缩到100k 之下
+	 * 
 	 * @param image
 	 * @return
 	 */
 	public Bitmap compress(Bitmap image) {
-            //图片允许最大空间   单位：KB
-            double maxSize =100.00;
-            //将bitmap放至数组中，意在bitmap的大小（与实际读取的原文件要大）  
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            image.compress(Bitmap.CompressFormat.PNG, 100, baos);
-            byte[] b = baos.toByteArray();
-            //将字节换成KB
-            double mid = b.length/1024;
-            //判断bitmap占用空间是否大于允许最大空间  如果大于则压缩 小于则不压缩
-            if (mid > maxSize) {
-                    //获取bitmap大小 是允许最大大小的多少倍
-                    double i = mid / maxSize;
-                    //开始压缩  此处用到平方根 将宽带和高度压缩掉对应的平方根倍 （1.保持刻度和高度和原bitmap比率一致，压缩后也达到了最大大小占用空间的大小）
-                    image = zoomImage(image, image.getWidth() / Math.sqrt(i),
-                    		image.getHeight() / Math.sqrt(i));
-            }
-            
-            return image;
+		// 图片允许最大空间 单位：KB
+		double maxSize = 50.00;
+		// 将bitmap放至数组中，意在bitmap的大小（与实际读取的原文件要大）
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		image.compress(Bitmap.CompressFormat.PNG, 100, baos);
+		byte[] b = baos.toByteArray();
+		// 将字节换成KB
+		double mid = b.length / 1024;
+		// 判断bitmap占用空间是否大于允许最大空间 如果大于则压缩 小于则不压缩
+		if (mid > maxSize) {
+			// 获取bitmap大小 是允许最大大小的多少倍
+			double i = mid / maxSize;
+			// 开始压缩 此处用到平方根 将宽带和高度压缩掉对应的平方根倍
+			// （1.保持刻度和高度和原bitmap比率一致，压缩后也达到了最大大小占用空间的大小）
+			image = zoomImage(image, image.getWidth() / Math.sqrt(i), image.getHeight() / Math.sqrt(i));
+		}
+
+		return image;
 
 	}
-	
-	
+
 	/***
-    * 图片的缩放方法
-    * 
-    * @param bgimage
-    *            ：源图片资源
-    * @param newWidth
-    *            ：缩放后宽度
-    * @param newHeight
-    *            ：缩放后高度
-    * @return
-    */
-   public static Bitmap zoomImage(Bitmap bgimage, double newWidth,
-                   double newHeight) {
-           // 获取这个图片的宽和高
-           float width = bgimage.getWidth();
-           float height = bgimage.getHeight();
-           // 创建操作图片用的matrix对象
-           Matrix matrix = new Matrix();
-           // 计算宽高缩放率
-           float scaleWidth = ((float) newWidth) / width;
-           float scaleHeight = ((float) newHeight) / height;
-           // 缩放图片动作
-           matrix.postScale(scaleWidth, scaleHeight);
-           Bitmap bitmap = Bitmap.createBitmap(bgimage, 0, 0, (int) width,
-                           (int) height, matrix, true);
-           return bitmap;
-   }
-
-
+	 * 图片的缩放方法
+	 * 
+	 * @param bgimage
+	 *            ：源图片资源
+	 * @param newWidth
+	 *            ：缩放后宽度
+	 * @param newHeight
+	 *            ：缩放后高度
+	 * @return
+	 */
+	public static Bitmap zoomImage(Bitmap bgimage, double newWidth, double newHeight) {
+		// 获取这个图片的宽和高
+		float width = bgimage.getWidth();
+		float height = bgimage.getHeight();
+		// 创建操作图片用的matrix对象
+		Matrix matrix = new Matrix();
+		// 计算宽高缩放率
+		float scaleWidth = ((float) newWidth) / width;
+		float scaleHeight = ((float) newHeight) / height;
+		// 缩放图片动作
+		matrix.postScale(scaleWidth, scaleHeight);
+		Bitmap bitmap = Bitmap.createBitmap(bgimage, 0, 0, (int) width, (int) height, matrix, true);
+		return bitmap;
+	}
 
 	public String compute32(byte[] content) {
 		StringBuffer buf = new StringBuffer("");
@@ -306,7 +304,7 @@ public class ImageClipActivity extends BaseActivity implements OnClickListener, 
 								if (cachePath != null && !"".equals(cachePath)) {
 									File file = new File(cachePath);
 									if (file.exists()) {
-										// file.delete();
+										file.delete();
 									}
 									cachePath = "";
 								}
