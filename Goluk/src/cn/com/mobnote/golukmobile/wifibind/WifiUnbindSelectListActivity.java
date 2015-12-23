43ab01b68e0b66ebb454825366da2f4f4a89af11
zenghao <zenghao@xiaocheben.com>
2015-12-23 11:40:24 +0800

    提交wifi绑定 UI流程

--- Goluk/src/cn/com/mobnote/golukmobile/wifibind/WifiUnbindSelectListActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/wifibind/WifiUnbindSelectListActivity.java
@@ -1,9 +1,13 @@
 package cn.com.mobnote.golukmobile.wifibind;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
 import cn.com.mobnote.golukmobile.BaseActivity;
 import cn.com.mobnote.golukmobile.R;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -18,7 +22,12 @@ public class WifiUnbindSelectListActivity extends BaseActivity implements OnClic
 	
 	/**没有数据时的默认布局**/
 	private RelativeLayout mEmptyLayout;
-
+	
+	/**编辑按钮**/
+	private Button mEditBtn;
+	
+	private WifiUnbindSelectListAdapter mListAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,6 +45,7 @@ public class WifiUnbindSelectListActivity extends BaseActivity implements OnClic
 		mListView = (ListView) findViewById(R.id.listView);
 		mEmptyLayout = (RelativeLayout) findViewById(R.id.emptyLayout);
 		mCloseBtn = (ImageView) findViewById(R.id.close_btn);
+		mEditBtn = (Button) findViewById(R.id.edit_btn);
 	}
 	
 	/**
@@ -43,11 +53,48 @@ public class WifiUnbindSelectListActivity extends BaseActivity implements OnClic
 	 */
 	private void initLisenner(){
 		mCloseBtn.setOnClickListener(this);
+		mEditBtn.setOnClickListener(this);
 	}
+	
+	JSONArray jsons = new JSONArray();
 	
 	/**初始化数据**/
 	private void initData(){
 		mListView.setEmptyView(mEmptyLayout);
+		mListAdapter = new WifiUnbindSelectListAdapter(this);
+		mListView.setAdapter(mListAdapter);
+		
+		
+		
+		
+		JSONObject json = new JSONObject();
+		json.put("type", "T1");
+		json.put("edit", 0);
+		json.put("name", "goluk12345678");
+		jsons.add(json);
+		
+		json = new JSONObject(); 
+		json.put("type", "G2");
+		json.put("edit", 0);
+		json.put("name", "goluk87654321");
+		jsons.add(json);
+		
+		json = new JSONObject(); 
+		json.put("type", "G1");
+		json.put("edit", 0);
+		json.put("name", "goluk00000000");
+		jsons.add(json);
+		
+		mListAdapter.setData(jsons);
+		mListAdapter.notifyDataSetChanged();
+	}
+	
+	/**
+	 * 添加头部
+	 * @param view
+	 */
+	public void addListViewHead(View view){
+		mListView.addHeaderView(view);
 	}
 
 
@@ -56,6 +103,16 @@ public class WifiUnbindSelectListActivity extends BaseActivity implements OnClic
 		switch (view.getId()) {
 		case R.id.close_btn:
 			this.finish();
+			break;
+		case R.id.edit_btn:
+			if(mListAdapter.mEditState){
+				mListAdapter.mEditState = false;
+				mEditBtn.setText(this.getResources().getString(R.string.edit_text));//编辑
+			}else{
+				mListAdapter.mEditState = true;
+				mEditBtn.setText(this.getResources().getString(R.string.user_personal_title_right));//保存
+			}
+			mListAdapter.notifyDataSetChanged();
 			break;
 		default:
 			break;
--- Goluk/src/cn/com/mobnote/golukmobile/wifibind/WifiUnbindSelectListAdapter.java
+++ Goluk/src/cn/com/mobnote/golukmobile/wifibind/WifiUnbindSelectListAdapter.java
@@ -8,6 +8,7 @@ import com.alibaba.fastjson.JSONObject;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
@@ -19,6 +20,13 @@ public class WifiUnbindSelectListAdapter extends BaseAdapter{
 	public Context mContext = null;
 	
 	public JSONArray mJsons = null;
+	
+	public View mHeadView = null;
+	
+	public HeadViewHodler  mHeadData = null;
+	
+	/**编辑按钮的状态 默认是不编辑的**/
+	public boolean mEditState = false;
 	
 	public WifiUnbindSelectListAdapter (Context context){
 		mContext = context;
@@ -49,7 +57,7 @@ public class WifiUnbindSelectListAdapter extends BaseAdapter{
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		JSONObject json = mJsons.getJSONObject(position);
+		final JSONObject json = mJsons.getJSONObject(position);
 		
 		ConnectViewHodler connectViewHodler = null;
 		if(json != null){
@@ -60,13 +68,74 @@ public class WifiUnbindSelectListAdapter extends BaseAdapter{
 				connectViewHodler.golukDelIcon = (ImageView) convertView.findViewById(R.id.goluk_del_icon);
 				connectViewHodler.golukIcon = (ImageView) convertView.findViewById(R.id.goluk_icon);
 				connectViewHodler.golukName = (TextView) convertView.findViewById(R.id.goluk_name);
+				connectViewHodler.golukConnLayout = (RelativeLayout) convertView.findViewById(R.id.goluk_conn_layout);
+				convertView.setTag(connectViewHodler);
 			}else{
 				connectViewHodler = (ConnectViewHodler) convertView.getTag();
 			}
+			if(json.getString("type").equals("G1")){
+				connectViewHodler.golukIcon.setImageResource(R.drawable.connect_g1_img);
+			}else if(json.getString("type").equals("G2")){
+				connectViewHodler.golukIcon.setImageResource(R.drawable.connect_g2_img);
+			}else if(json.getString("type").equals("G1S")){
+				connectViewHodler.golukIcon.setImageResource(R.drawable.connect_g1_img);
+			}else if(json.getString("type").equals("T1")){
+				connectViewHodler.golukIcon.setImageResource(R.drawable.connect_t1_img);
+			}
 			
+			connectViewHodler.golukName.setText(json.getString("name"));
 			
+			if(mEditState == false){
+				connectViewHodler.golukDelIcon.setVisibility(View.GONE);
+			}else{
+				connectViewHodler.golukDelIcon.setVisibility(View.VISIBLE);
+			}
+			if(position ==0){
+				connectViewHodler.historyTxt.setVisibility(View.VISIBLE);
+			}else{
+				connectViewHodler.historyTxt.setVisibility(View.GONE);
+			}
+			
+			connectViewHodler.golukDelIcon.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View view) {
+					mJsons.remove(json);
+					notifyDataSetChanged();
+				}
+			});
+			
+			connectViewHodler.golukConnLayout.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					if(mEditState == false){
+						if(mHeadView == null){
+							mHeadView = LayoutInflater.from(mContext).inflate(R.layout.unbind_connection_head,null);
+							WifiUnbindSelectListActivity wsla = (WifiUnbindSelectListActivity) mContext;
+							wsla.addListViewHead(mHeadView);
+						}
+						
+						if(mHeadData == null){
+							mHeadData = new HeadViewHodler();
+							mHeadData.connHeadIcon = (ImageView) mHeadView.findViewById(R.id.conn_head_icon);
+							mHeadData.connTxt = (TextView) mHeadView.findViewById(R.id.conn_txt);
+							mHeadData.golukDelIcon =(ImageView) mHeadView.findViewById(R.id.goluk_del_icon);
+							mHeadData.golukIcon  = (ImageView) mHeadView.findViewById(R.id.goluk_icon);
+							mHeadData.golukName = (TextView) mHeadView.findViewById(R.id.goluk_name);
+						}
+						
+						mHeadData.golukName.setText(json.getString("name"));
+						mHeadView.setTag(mHeadData);
+						
+						mJsons.remove(json);
+						notifyDataSetChanged();
+					}
+					
+				}
+			});
 		}
-		return null;
+		return convertView;
 	}
 	
 	
@@ -75,6 +144,15 @@ public class WifiUnbindSelectListAdapter extends BaseAdapter{
 		 ImageView golukDelIcon;
 		 ImageView golukIcon;
 		 TextView golukName; 
+		 RelativeLayout golukConnLayout;
+	}
+	
+	public static class HeadViewHodler{
+		 TextView connTxt;
+		 ImageView connHeadIcon;
+		 ImageView golukIcon;
+		 TextView golukName; 
+		 ImageView golukDelIcon;
 	}
 
 
