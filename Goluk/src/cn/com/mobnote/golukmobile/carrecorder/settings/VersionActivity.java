f6e2107f8c8c87feaadbee6c9e8db43811fec67f
jiayf <jiayufeng@xiaocheben.com>
2016-01-06 14:11:29 +0800

    修改版本信息显示T1S设备的图片不确的问题

--- Goluk/src/cn/com/mobnote/golukmobile/carrecorder/settings/VersionActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/carrecorder/settings/VersionActivity.java
@@ -22,111 +22,92 @@ import cn.com.mobnote.golukmobile.carrecorder.util.SoundUtils;
 import cn.com.mobnote.module.ipcmanager.IPCManagerFn;
 import cn.com.tiros.debug.GolukDebugUtils;
 
- /**
-  * 1.编辑器必须显示空白处
-  *
-  * 2.所有代码必须使用TAB键缩进
-  *
-  * 3.类首字母大写,函数、变量使用驼峰式命名,常量所有字母大写
-  *
-  * 4.注释必须在行首写.(枚举除外)
-  *
-  * 5.函数使用块注释,代码逻辑使用行注释
-  *
-  * 6.文件头部必须写功能说明
-  *
-  * 7.所有代码文件头部必须包含规则说明
-  *
-  * 版本信息
-  *
-  * 2015年4月9日
-  *
-  * @author xuhw
-  */
+/**
+ *
+ * 版本信息
+ *
+ * 2015年4月9日
+ *
+ * @author xuhw
+ */
 @SuppressLint("InflateParams")
-public class VersionActivity extends CarRecordBaseActivity implements IPCManagerFn{
+public class VersionActivity extends CarRecordBaseActivity implements IPCManagerFn {
 	/** Goluk设备编号 */
-	private TextView mDeviceId=null;
+	private TextView mDeviceId = null;
 	/** 固件版本号 */
-	private TextView mVersion=null;
-	/**ipc设备型号**/
+	private TextView mVersion = null;
+	/** ipc设备型号 **/
 	private TextView mTextIpcModel = null;
 	/****/
 	private String mIpcModelName = "";
 	private ImageView mIPCImage = null;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		addContentView(LayoutInflater.from(this).inflate(R.layout.carrecorder_version, null)); 
+		addContentView(LayoutInflater.from(this).inflate(R.layout.carrecorder_version, null));
 		setTitle("版本信息");
-		
-		if(null != GolukApplication.getInstance().getIPCControlManager()){
+
+		if (null != GolukApplication.getInstance().getIPCControlManager()) {
 			GolukApplication.getInstance().getIPCControlManager().addIPCManagerListener("carversion", this);
 		}
 		mIpcModelName = GolukApplication.getInstance().mIPCControlManager.mProduceName;
-		mDeviceId = (TextView)findViewById(R.id.mDeviceId);
-		mVersion = (TextView)findViewById(R.id.mVersion);
+		mDeviceId = (TextView) findViewById(R.id.mDeviceId);
+		mVersion = (TextView) findViewById(R.id.mVersion);
 		mTextIpcModel = (TextView) findViewById(R.id.text_model);
 		mIPCImage = (ImageView) findViewById(R.id.im_carrecorder_version_icon);
-		
+
 		mDeviceId.setText("");
 		mVersion.setText("");
-		mTextIpcModel.setText("极路客"+mIpcModelName);
-		if (IPCControlManager.T1_SIGN.equals(GolukApplication.getInstance().getIPCControlManager().mProduceName)) {
+		mTextIpcModel.setText("极路客" + mIpcModelName);
+		if (IPCControlManager.T1_SIGN.equals(mIpcModelName) || IPCControlManager.T1s_SIGN.equals(mIpcModelName)) {
 			mIPCImage.setImageResource(R.drawable.connect_t1_icon_1);
 		} else {
 			mIPCImage.setImageResource(R.drawable.ipc);
 		}
-		if(GolukApplication.getInstance().getIpcIsLogin()){
+		if (GolukApplication.getInstance().getIpcIsLogin()) {
 			boolean a = GolukApplication.getInstance().getIPCControlManager().getIPCIdentity();
-			GolukDebugUtils.e("xuhw","YYYYYY=======getIPCIdentity============a="+a);
-			
+			GolukDebugUtils.e("xuhw", "YYYYYY=======getIPCIdentity============a=" + a);
+
 			boolean v = GolukApplication.getInstance().getIPCControlManager().getVersion();
-			GolukDebugUtils.e("xuhw","YYYYYY=======getVersion============v="+v);
+			GolukDebugUtils.e("xuhw", "YYYYYY=======getVersion============v=" + v);
 		}
-		
+
 		float density = SoundUtils.getInstance().getDisplayMetrics().density;
 		int screedWidth = SoundUtils.getInstance().getDisplayMetrics().widthPixels;
-		LinearLayout idlayout = (LinearLayout)findViewById(R.id.idlayout);
-		LinearLayout vsnlayout = (LinearLayout)findViewById(R.id.vsnlayout);
-		int paddingLeft = (int)(screedWidth/2 - 85*density);
-		idlayout.setPadding(paddingLeft, (int)(58*density), 0, 0);
-		vsnlayout.setPadding(paddingLeft, (int)(6*density), 0, 0);
-		
-	}
-	
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		
+		LinearLayout idlayout = (LinearLayout) findViewById(R.id.idlayout);
+		LinearLayout vsnlayout = (LinearLayout) findViewById(R.id.vsnlayout);
+		int paddingLeft = (int) (screedWidth / 2 - 85 * density);
+		idlayout.setPadding(paddingLeft, (int) (58 * density), 0, 0);
+		vsnlayout.setPadding(paddingLeft, (int) (6 * density), 0, 0);
+
 	}
 
 	@Override
 	public void IPCManage_CallBack(int event, int msg, int param1, Object param2) {
-		GolukDebugUtils.e("xuhw", "YYYYYY====IPC_VDCP_Msg_GetIdentity====msg="+msg+"===param1="+param1+"==param2="+param2);
-		
-		if(event == ENetTransEvent_IPC_VDCP_CommandResp){
-			if(msg == IPC_VDCP_Msg_GetIdentity){
-				if(param1 == RESULE_SUCESS){
-					final IPCIdentityState mVersionState = IpcDataParser.parseVersionState((String)param2);
-					if(null != mVersionState){
+		GolukDebugUtils.e("xuhw", "YYYYYY====IPC_VDCP_Msg_GetIdentity====msg=" + msg + "===param1=" + param1
+				+ "==param2=" + param2);
+
+		if (event == ENetTransEvent_IPC_VDCP_CommandResp) {
+			if (msg == IPC_VDCP_Msg_GetIdentity) {
+				if (param1 == RESULE_SUCESS) {
+					final IPCIdentityState mVersionState = IpcDataParser.parseVersionState((String) param2);
+					if (null != mVersionState) {
 						runOnUiThread(new Runnable() {
 							@Override
 							public void run() {
-//								mDeviceId.setText(""+mVersionState.code);
 								mDeviceId.setText(mVersionState.name);
 							}
 						});
 					}
 				}
-			}else if(IPC_VDCP_Msg_GetVersion == msg){
-				if(param1 == RESULE_SUCESS){
-					String str = (String)param2;
-					if(TextUtils.isEmpty(str)){
+			} else if (IPC_VDCP_Msg_GetVersion == msg) {
+				if (param1 == RESULE_SUCESS) {
+					String str = (String) param2;
+					if (TextUtils.isEmpty(str)) {
 						return;
 					}
-					
+
 					try {
 						JSONObject json = new JSONObject(str);
 						String version = json.optString("version");
@@ -134,44 +115,43 @@ public class VersionActivity extends CarRecordBaseActivity implements IPCManager
 					} catch (JSONException e) {
 						e.printStackTrace();
 					}
-					
+
 				}
 			}
 		}
-		
+
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 		GolukApplication.getInstance().setContext(this, "carrecordversion");
 	}
-	
-	public void exit(){
-		if(null != GolukApplication.getInstance().getIPCControlManager()){
+
+	public void exit() {
+		if (null != GolukApplication.getInstance().getIPCControlManager()) {
 			GolukApplication.getInstance().getIPCControlManager().removeIPCManagerListener("carversion");
 		}
 		finish();
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-			case R.id.back_btn:
-				exit(); 
-				break;
-				
-			default:
-				break;
+		case R.id.back_btn:
+			exit();
+			break;
+		default:
+			break;
 		}
-	}  
-	
+	}
+
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-    	if(keyCode==KeyEvent.KEYCODE_BACK){
-    		exit(); 
-        	return true;
-        }else
-        	return super.onKeyDown(keyCode, event); 
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			exit();
+			return true;
+		} else
+			return super.onKeyDown(keyCode, event);
 	}
 
 }
