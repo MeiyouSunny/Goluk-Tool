58bae3c8273bc7d0a791dccbc3d0c66fcd8fc3f5
lily <liliyue@xiaocheben.com>
2016-01-08 15:15:07 +0800

    T1点击精彩抢拍时状态修改

--- Goluk/src/cn/com/mobnote/golukmobile/carrecorder/CarRecorderActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/carrecorder/CarRecorderActivity.java
@@ -1397,7 +1397,6 @@ public class CarRecorderActivity extends BaseActivity implements OnClickListener
 			mHandler.sendEmptyMessageDelayed(CarRecorderActivity.QUERYFILEEXIT, CarRecorderActivity.QUERYFILETIME);
 		}
 		mShootTime = 0;
-		isRecording = false;
 		m8sBtn.setBackgroundResource(R.drawable.driving_car_living_defalut_icon);
 		if (null != m8sTimer) {
 			m8sTimer.cancel();
