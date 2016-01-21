1e1e6dd59209738a1f0091644fa211ceb4b8c4b7
lizhongbo <lizhongbo@goluk.com>
2016-01-21 19:45:42 +0800

    fix mantis bug id: 1035

--- Goluk/src/cn/com/mobnote/golukmobile/msg/MessageCenterActivity.java
+++ Goluk/src/cn/com/mobnote/golukmobile/msg/MessageCenterActivity.java
@@ -270,20 +270,21 @@ public class MessageCenterActivity extends BaseActivity implements OnClickListen
 	public void onLoadComplete(int requestType, Object result) {
 		mScrollView.onRefreshComplete();
 		if(null == result) {
+			Toast.makeText(this, getString(R.string.network_error), Toast.LENGTH_SHORT).show();
 			return;
 		}
 
 		if(requestType == IPageNotifyFn.PageType_MsgCounter) {
 			MessageCounterBean bean = (MessageCounterBean)result;
 			if(null == bean.data) {
+				Toast.makeText(this, getString(
+						R.string.str_server_request_unknown_error), Toast.LENGTH_SHORT).show();
 				return;
 			}
 
 			if(!bean.success) {
-				return;
-			}
-
-			if(null == bean.data) {
+				Toast.makeText(this, getString(
+						R.string.str_server_request_arg_error), Toast.LENGTH_SHORT).show();
 				return;
 			}
 
