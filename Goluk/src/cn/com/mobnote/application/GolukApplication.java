343901ede058ca13fc4bcdf0dfeefdd42557be17
jiayf <jiayufeng@xiaocheben.com>
2016-01-25 16:40:58 +0800

    文件下载失败，删除数据库信息

--- Goluk/src/cn/com/mobnote/application/GolukApplication.java
+++ Goluk/src/cn/com/mobnote/application/GolukApplication.java
@@ -709,6 +709,7 @@ public class GolukApplication extends Application implements IPageNotifyFn, IPCM
 		}
 
 		try {
+			GolukDebugUtils.e("","GolukApplication-----ipcVideoDownLoadCallback:  success:" + success + "  data:" + data);
 			JSONObject jsonobj = new JSONObject(data);
 			String tag = jsonobj.optString("tag");
 			if (tag.equals("videodownload")) {
@@ -799,13 +800,15 @@ public class GolukApplication extends Application implements IPageNotifyFn, IPCM
 					resetSDCheckState();
 					GolukDebugUtils.e("xuhw", "YYYYYY=＠＠＠＠===download==fail===success=" + success + "==data=" + data);
 					JSONObject json = new JSONObject(data);
-					String filename = json.optString("filename");
+					final String filename = json.optString("filename");
 
 					if (mDownLoadFileList.contains(filename)) {
 						if (!mNoDownLoadFileList.contains(filename)) {
 							mNoDownLoadFileList.add(filename);
 						}
 					}
+					// 下载文件失败，删除数据库中的信息
+					GolukVideoInfoDbManager.getInstance().delVideoInfo(filename);
 
 					GolukDebugUtils.e("xuhw", "BBBBBBB=======down==fail====" + mNoDownLoadFileList.size());
 					if (checkDownloadCompleteState()) {
