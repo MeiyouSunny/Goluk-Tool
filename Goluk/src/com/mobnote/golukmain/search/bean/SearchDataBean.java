2f0c878542cc6b5785f008c9553967b9752e9ae5
leege100 <wangli@goluk.com>
2016-04-27 16:16:18 +0800

    修复mantics上第0001819个bug(输入表情和符号crash)

--- Goluk/src/com/mobnote/golukmain/search/bean/SearchDataBean.java
+++ Goluk/src/com/mobnote/golukmain/search/bean/SearchDataBean.java
@@ -3,14 +3,14 @@ package com.mobnote.golukmain.search.bean;
 import java.util.List;
 
 import com.alibaba.fastjson.annotation.JSONField;
-import com.mobnote.golukmain.following.bean.FollowingItemBean;
+import com.mobnote.golukmain.userbase.bean.SimpleUserItemBean;
 
 public class SearchDataBean {
 
 	@JSONField(name="userlist")
-	public List<FollowingItemBean> userlist;
+	public List<SimpleUserItemBean> userlist;
 	@JSONField(name="usercount")
 	public int usercount;
 	@JSONField(name="recomlist")
-	public List<FollowingItemBean> recomlist;
+	public List<SimpleUserItemBean> recomlist;
 }
