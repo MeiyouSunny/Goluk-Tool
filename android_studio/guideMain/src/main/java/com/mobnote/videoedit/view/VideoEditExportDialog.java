48d2f71c809ecf17630df1afab53936091d5cabb
lizhongbo <lizhongbo@goluk.com>
2016-05-12 17:01:27 +0800

    fix mantis bug id: 1997

--- android_studio/guideMain/src/main/java/com/mobnote/videoedit/view/VideoEditExportDialog.java
+++ android_studio/guideMain/src/main/java/com/mobnote/videoedit/view/VideoEditExportDialog.java
@@ -73,7 +73,7 @@ public class VideoEditExportDialog extends Dialog implements android.view.View.O
             mAEActivity.exportAfterEffectVideo(VideoEditConstant.EXPORT_HD720P_WIDTH, VideoEditConstant.EXPORT_HD720P_HEIGHT);
         } else if (id == R.id.ll_ae_video_export_dialog_480p) {
             mAEActivity.playOrPause();
-            mAEActivity.exportAfterEffectVideo(VideoEditConstant.EXPORT_HD720P_WIDTH, VideoEditConstant.EXPORT_HD720P_HEIGHT);
+            mAEActivity.exportAfterEffectVideo(VideoEditConstant.EXPORT_480P_WIDTH, VideoEditConstant.EXPORT_480P_HEIGHT);
         } else if (id == R.id.tv_ae_video_export_cancel) {
 //            dismiss();
         }
