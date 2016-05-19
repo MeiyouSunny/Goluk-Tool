6dd30883c2f123afe2331f35d88b668ad127c017
lizhongbo <lizhongbo@goluk.com>
2016-05-19 11:41:58 +0800

    chang music select strategy

--- android_studio/guideMain/src/main/java/com/mobnote/videoedit/bean/AEMusic.java
+++ android_studio/guideMain/src/main/java/com/mobnote/videoedit/bean/AEMusic.java
@@ -1,39 +1,58 @@
 package com.mobnote.videoedit.bean;
 
 public class AEMusic {
+    boolean isSelected;
+    String musicName;
+    String musicPath;
+    int musicCoverNormal;
+    int musicCoverSelected;
 
-	boolean isSelected;
-	String musicName;
-	String musicPath;
+    public int getMusicCoverNormal() {
+        return musicCoverNormal;
+    }
 
-	public AEMusic(String name, String path, boolean selected) {
-		this.musicName = name;
-		this.musicPath = path;
-		this.isSelected = selected;
-	}
+    public void setMusicCoverNormal(int musicCoverNormal) {
+        this.musicCoverNormal = musicCoverNormal;
+    }
 
-	public boolean isSelected() {
-		return isSelected;
-	}
+    public int getMusicCoverSelected() {
+        return musicCoverSelected;
+    }
 
-	public void setSelected(boolean isSelected) {
-		this.isSelected = isSelected;
-	}
+    public void setMusicCoverSelected(int musicCoverSelected) {
+        this.musicCoverSelected = musicCoverSelected;
+    }
 
-	public String getMusicName() {
-		return musicName;
-	}
+    public AEMusic(String name, String path, boolean selected, int coverNormal, int coverSelected) {
+        this.musicName = name;
+        this.musicPath = path;
+        this.isSelected = selected;
+        musicCoverNormal = coverNormal;
+        musicCoverSelected = coverSelected;
+    }
 
-	public void setMusicName(String musicName) {
-		this.musicName = musicName;
-	}
+    public boolean isSelected() {
+        return isSelected;
+    }
 
-	public String getMusicPath() {
-		return musicPath;
-	}
+    public void setSelected(boolean isSelected) {
+        this.isSelected = isSelected;
+    }
 
-	public void setMusicPath(String musicPath) {
-		this.musicPath = musicPath;
-	}
+    public String getMusicName() {
+        return musicName;
+    }
+
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
+
+    public String getMusicPath() {
+        return musicPath;
+    }
+
+    public void setMusicPath(String musicPath) {
+        this.musicPath = musicPath;
+    }
 
 }
