122b79320aa97f5e85ce0fe841ea6fb3f5df22f1
lily <liliyue@xiaocheben.com>
2016-05-27 15:15:37 +0800

    个人主页查看大图

--- android_studio/guideMain/src/main/java/com/mobnote/golukmain/usercenter/UserLargeHeadActivity.java
+++ android_studio/guideMain/src/main/java/com/mobnote/golukmain/usercenter/UserLargeHeadActivity.java
@@ -4,16 +4,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
+
 import com.mobnote.golukmain.R;
 import com.mobnote.user.UserUtils;
 import com.mobnote.util.GlideUtils;
 
 import cn.com.tiros.debug.GolukDebugUtils;
+import uk.co.senab.photoview.PhotoView;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class UserLargeHeadActivity extends Activity {
 
-    private LargeHeadView mLargeHeadView;
+    private PhotoView mPhotoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +25,31 @@ public class UserLargeHeadActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_user_large_head);
 
-        mLargeHeadView = (LargeHeadView) findViewById(R.id.lv_usercenter_large_head_view);
+        mPhotoView = (PhotoView) findViewById(R.id.lv_usercenter_large_head_view);
 
         Intent it = getIntent();
         String head = it.getStringExtra("headurl");
         GolukDebugUtils.e("", "UserLargeHeadActivity---------head: " + head);
         if (head.contains("http")) {
-            GlideUtils.loadImage(this, mLargeHeadView, head, R.drawable.usercenter_head_default);
+            GlideUtils.loadImage(this, mPhotoView, head, R.drawable.usercenter_head_default);
         } else {
-            UserUtils.focusHead(this, head, mLargeHeadView);
+            UserUtils.focusHead(this, head, mPhotoView);
         }
+
+        mPhotoView.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
+            @Override
+            public void onPhotoTap(View view, float v, float v1) {
+                exit();
+            }
+        });
+
+        mPhotoView.setOnViewTapListener(new PhotoViewAttacher.OnViewTapListener() {
+            @Override
+            public void onViewTap(View view, float v, float v1) {
+                exit();
+            }
+        });
+
     }
 
 
@@ -48,4 +67,5 @@ public class UserLargeHeadActivity extends Activity {
         }
         return super.onKeyDown(keyCode, event);
     }
+
 }
