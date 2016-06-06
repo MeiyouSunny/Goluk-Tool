8f513c3fe6b6a3e5f54642d944646d97698644ef
lizhongbo <lizhongbo@goluk.com>
2016-06-06 15:59:36 +0800

    zhuge io for player

--- android_studio/guideMain/src/main/java/com/mobnote/golukmain/photoalbum/LoopFragment.java
+++ android_studio/guideMain/src/main/java/com/mobnote/golukmain/photoalbum/LoopFragment.java
@@ -422,6 +422,9 @@ public class LoopFragment extends Fragment implements IPCManagerFn {
 	private void gotoVideoPlayPage(int from, String path, String filename , String createTime, String videoHP, String size) {
 		if (!isShowPlayer) {
 			isShowPlayer = true;
+            ZhugeUtils.eventAlbumPlayer(getActivity(),
+                    getString(R.string.str_zhuge_video_player_recycle),
+                    getString(R.string.str_zhuge_video_player_recycle));
 			GolukUtils.startPhotoAlbumPlayerActivity(LoopFragment.this.getContext(),PhotoAlbumConfig.PHOTO_BUM_IPC_LOOP,"ipc",path,filename,createTime,videoHP,size);
 		}
 	}
