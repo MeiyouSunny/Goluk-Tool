b06225094e198ed620a2da4274d9fa9a13734d4a
lizhongbo <lizhongbo@goluk.com>
2016-06-16 14:31:02 +0800

    close loading dialog if video bitmap generate failed in aftereffect

--- android_studio/guideMain/src/main/java/com/mobnote/videoedit/AfterEffectActivity.java
+++ android_studio/guideMain/src/main/java/com/mobnote/videoedit/AfterEffectActivity.java
@@ -733,6 +733,13 @@ public class AfterEffectActivity extends BaseActivity implements AfterEffectList
         case MSG_AE_BITMAP_READ_FAILED: {
             Log.d(TAG, "MSG_AE_BITMAP_READ_FAILED");
             ZhugeUtils.eventAddChunk(this, false);
+            if(null != mFullLoadingDialog && mFullLoadingDialog.isShowing()) {
+                mFullLoadingDialog.close();
+            }
+            Toast.makeText(this, getString(R.string.str_ae_add_chunk_failed), Toast.LENGTH_SHORT).show();
+            if(mProjectItemList == null || mProjectItemList.size() <= 3) {
+                finishAfterEffect();
+            }
         }
             break;
         case MSG_AE_CHUNK_PLAY_END: {
