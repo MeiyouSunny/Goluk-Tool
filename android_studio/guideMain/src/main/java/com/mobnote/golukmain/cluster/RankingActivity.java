31136f63519a4c44585b7383c1d05e41d0c58ead
zenghao <zenghao@xiaocheben.com>
2016-06-17 15:02:03 +0800

    1:update bug

--- android_studio/guideMain/src/main/java/com/mobnote/golukmain/cluster/RankingActivity.java
+++ android_studio/guideMain/src/main/java/com/mobnote/golukmain/cluster/RankingActivity.java
@@ -76,7 +76,7 @@ public class RankingActivity extends BaseActivity implements IRequestResultListe
     @Override
     protected void onResume() {
         super.onResume();
-        getRankingData("0","");//首次进入请求数据
+//        getRankingData("0","");//首次进入请求数据
     }
 
     private void initListener(){
@@ -136,7 +136,7 @@ public class RankingActivity extends BaseActivity implements IRequestResultListe
         mRTPullListView.setSelector(new ColorDrawable(Color.TRANSPARENT));
         mRTPullListView.setAdapter(mRrankingAdapter);
         mRTPullListView.firstFreshState();
-//        getRankingData("0","");//首次进入请求数据
+        getRankingData("0","");//首次进入请求数据
     }
 
     private void getRankingData(String operation,String timestamp){
