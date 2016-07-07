2e7fbe9f37e7052ba29b19bb6bd726b7aa535224
lily <liliyue@xiaocheben.com>
2016-07-07 16:17:10 +0800

    修改T1设置项中根据固件版本来显示视频水印和语言设置这两项

--- android_studio/guideMain/src/main/java/com/mobnote/golukmain/carrecorder/settings/TSettingsActivity.java
+++ android_studio/guideMain/src/main/java/com/mobnote/golukmain/carrecorder/settings/TSettingsActivity.java
@@ -490,7 +490,7 @@ public class TSettingsActivity extends BaseActivity implements OnClickListener,I
 		}
 //		findViewById(R.id.ly_t_settings_buy).setVisibility(View.VISIBLE);
 		//国际版不显示购买链接、视频水印、语言设置
-		if (GolukApplication.getInstance().isMainland()) {
+		if (!GolukApplication.getInstance().mIpcVersion.toLowerCase().startsWith("t1u")) {
 			findViewById(R.id.ly_t_settings_buy).setVisibility(View.VISIBLE);
 			mVideoLogoLayout.setVisibility(View.VISIBLE);
 			mLanguageLayout.setVisibility(View.VISIBLE);
