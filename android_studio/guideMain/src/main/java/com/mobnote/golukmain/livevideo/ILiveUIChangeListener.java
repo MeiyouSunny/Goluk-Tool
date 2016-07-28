afa847ecc785064e832fc39e9f2c678eaeebd146
leege100 <wangli@goluk.com>
2016-07-28 14:44:08 +0800

    update live UI

---
+++ android_studio/guideMain/src/main/java/com/mobnote/golukmain/livevideo/ILiveUIChangeListener.java
@@ -0,0 +1,8 @@
+package com.mobnote.golukmain.livevideo;
+
+/**
+ * Created by leege100 on 2016/7/28.
+ */
+public interface ILiveUIChangeListener {
+    public void onFramgentTopMarginReceived(int topMargin);
+}
