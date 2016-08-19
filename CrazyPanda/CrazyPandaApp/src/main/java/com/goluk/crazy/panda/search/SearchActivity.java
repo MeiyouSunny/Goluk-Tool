f8282861f2599ce9c616e066aae039bc052893c2
leege100 <wangli@goluk.com>
2016-08-19 18:20:28 +0800

    add HeaderView

--- CrazyPanda/CrazyPandaApp/src/main/java/com/goluk/crazy/panda/search/SearchActivity.java
+++ CrazyPanda/CrazyPandaApp/src/main/java/com/goluk/crazy/panda/search/SearchActivity.java
@@ -1,15 +1,17 @@
 package com.goluk.crazy.panda.search;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.goluk.crazy.panda.R;
 import com.goluk.crazy.panda.common.activity.BaseActivity;
 import com.goluk.crazy.panda.common.http.SearchService;
 import com.goluk.crazy.panda.common.http.bean.HttpResultBean;
+import com.goluk.crazy.panda.common.widget.HeaderBar;
 import com.goluk.crazy.panda.search.bean.SearchDataBean;
 import com.goluk.crazy.panda.search.bean.SearchUserInfoBean;
 
@@ -36,6 +38,8 @@ public class SearchActivity extends BaseActivity {
     TextView tvUsername;
 
     StringBuilder mUserNameStrBuilder;
+    @BindView(R.id.headerbar_search)
+    HeaderBar mSearchHeaderbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +48,25 @@ public class SearchActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         String baseUrl = "http://server.goluk.cn/cdcSearch/";
+
+        mSearchHeaderbar.setOnLeftClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SearchActivity.this,"左边被点击",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mSearchHeaderbar.setOnRightClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SearchActivity.this,"右边被点击",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mSearchHeaderbar.setOnCenterClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SearchActivity.this,"中间被点击",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         mUserNameStrBuilder = new StringBuilder();
         mRetrofit = new Retrofit.Builder()
