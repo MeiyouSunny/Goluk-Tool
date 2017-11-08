372ef4a698cd1aa0c344215b4f91385f870b4923
ShangeJi <shangji@goluk.com>
2017-11-08 15:16:47 +0800

    Revert "Add T1SP connect WIFI"
    This reverts commit 2305d51179149b3304d615daa82ebacf1e32dbd5.

--- android_studio/guideMain/src/main/java/com/mobnote/golukmain/wifibind/WiFiLinkListActivity.java
+++ android_studio/guideMain/src/main/java/com/mobnote/golukmain/wifibind/WiFiLinkListActivity.java
@@ -525,19 +525,17 @@ public class WiFiLinkListActivity extends BaseActivity implements OnClickListene
     protected void onResume() {
         mApp.setContext(this, "WiFiLinkList");
         super.onResume();
-        // T1SP
+//        if(!mAutoConn){
+//            mStartSystemWifi = true;
+//            GolukUtils.startSystemWifiList(this);
+//            mAutoConn = true;
+//            return;
+//        }
+//        collectLog("onResume", "----1:");
+//        if (WifiBindDataCenter.getInstance().isHasDataHistory() || mStartSystemWifi)
+//            autoConnWifi();
+//        mIsCanAcceptNetState = true;
         connect();
-
-        if(!mAutoConn){
-            mStartSystemWifi = true;
-            GolukUtils.startSystemWifiList(this);
-            mAutoConn = true;
-            return;
-        }
-        collectLog("onResume", "----1:");
-        if (WifiBindDataCenter.getInstance().isHasDataHistory() || mStartSystemWifi)
-            autoConnWifi();
-        mIsCanAcceptNetState = true;
     }
 
     private void connect() {
