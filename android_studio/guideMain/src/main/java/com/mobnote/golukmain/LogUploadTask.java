e3a3eadfc6541f2737da3ca4c36e22759a71d8f4
shangji <shangji@goluk.com>
2017-11-14 16:24:39 +0800

    fix 7452: add some params before upload log

--- android_studio/guideMain/src/main/java/com/mobnote/golukmain/LogUploadTask.java
+++ android_studio/guideMain/src/main/java/com/mobnote/golukmain/LogUploadTask.java
@@ -5,8 +5,12 @@ import android.text.TextUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.mobnote.application.GolukApplication;
 import com.mobnote.golukmain.http.HttpManager;
 import com.mobnote.golukmain.userlogin.UploadUtil;
+import com.mobnote.map.LngLat;
+import com.mobnote.util.GolukUtils;
+import com.mobnote.util.SharedPrefUtil;
 
 import java.io.DataOutputStream;
 import java.io.File;
@@ -20,6 +24,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import cn.com.mobnote.logic.GolukModule;
+import cn.com.mobnote.module.page.IPageNotifyFn;
+import cn.com.tiros.api.Tapi;
 import cn.com.tiros.debug.GolukDebugUtils;
 
 /**
@@ -42,17 +49,41 @@ public class LogUploadTask extends AsyncTask<String, Integer, String> {
 
     @Override
     protected String doInBackground(String... params) {
-
         File file = new File(filePath);
         if (!file.exists()) {
             return null;
         }
-
         Map<String, String> requestParams = new HashMap<String, String>();
         requestParams.put("xieyi", "200");
-        requestParams.put("commuid", uid);
-        requestParams.put("commmid", mid);
-
+        GolukApplication app = GolukApplication.getInstance();
+        if (app.mGoluk != null) {
+            String verName = GolukApplication.getInstance().mGoluk.GolukLogicCommGet(GolukModule.Goluk_Module_HttpPage,
+                    IPageNotifyFn.PageType_GetVersion, "fs6:/version");
+            requestParams.put("commappversion", verName);
+        } else {
+            requestParams.put("commappversion", "");
+        }
+        if (app.mIPCControlManager != null) {
+            requestParams.put("commhdtype", GolukApplication.getInstance().mIPCControlManager.mProduceName);
+        } else {
+            requestParams.put("commhdtype", "");
+        }
+        requestParams.put("commipcversion", SharedPrefUtil.getIPCVersion());
+        requestParams.put("commdevmodel", android.os.Build.MODEL);
+        requestParams.put("commlat", "" + LngLat.lat);
+        requestParams.put("commlon", "" + LngLat.lng);
+        requestParams.put("commmid", "" + Tapi.getMobileId());
+        requestParams.put("commostag", "android");
+        requestParams.put("commosversion", android.os.Build.VERSION.RELEASE);
+        requestParams.put("commticket", SharedPrefUtil.getUserToken());
+        String uid = GolukApplication.getInstance().mCurrentUId;
+        if (TextUtils.isEmpty(uid)) {
+            requestParams.put("commuid", "");
+        } else {
+            requestParams.put("commuid", uid);
+        }
+        requestParams.put("commversion", GolukUtils.getCommversion());
+        requestParams.put("commlocale", GolukUtils.getLanguageAndCountry());
         String result = null;
         try {
             result = UploadUtil.uploadFile(mRequestUrl, requestParams, file);
