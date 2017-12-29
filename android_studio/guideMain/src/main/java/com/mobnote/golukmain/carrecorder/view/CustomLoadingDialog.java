60428e2a91d79d5855babcef1ee873b6c0c807aa
HuangJW <huangjingwei@goluk.com>
2017-12-29 15:49:48 +0800

    Fix bug 0008278

--- android_studio/guideMain/src/main/java/com/mobnote/golukmain/carrecorder/view/CustomLoadingDialog.java
+++ android_studio/guideMain/src/main/java/com/mobnote/golukmain/carrecorder/view/CustomLoadingDialog.java
@@ -1,106 +1,110 @@
 package com.mobnote.golukmain.carrecorder.view;
-import cn.com.tiros.debug.GolukDebugUtils;
-import android.app.AlertDialog;
+
+import android.app.Activity;
 import android.app.ProgressDialog;
-import com.mobnote.golukmain.R;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnKeyListener;
-import android.content.DialogInterface.OnShowListener;
-import android.graphics.drawable.AnimationDrawable;
 import android.view.KeyEvent;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import com.mobnote.golukmain.R;
+
+import cn.com.tiros.debug.GolukDebugUtils;
 
 public class CustomLoadingDialog {
-	ForbidBack forbidInterface;
-	private ProgressDialog mDialog;
-	private String mMessage;
+    ForbidBack forbidInterface;
+    private ProgressDialog mDialog;
+    private String mMessage;
+    private Context mContext;
 
-	public CustomLoadingDialog(Context context, String txt) {
-		mDialog = new ProgressDialog(context,R.style.CustomDialog);
-		if (!"".equals(txt) && txt != null) {
-			mMessage = txt;
-		}else{
-			mMessage = context.getResources().getString(R.string.str_loading_text);
-		}
-		mDialog.setMessage(mMessage);
-	}
-
-	public void show(){
-		if(mDialog != null){
-			if(!mDialog.isShowing()){
-				mDialog.show();
-			}
-		}
-	}
-
-	public boolean isShowing() {
-		if (null != mDialog) {
-			return mDialog.isShowing();
-		}
-		return false;
-	}
-
-	public void close() {
-		if (mDialog != null) {
-			if (mDialog.isShowing()) {
-				mDialog.dismiss();
-			}
-		}
-	}
-
-	public void setListener(final ForbidBack forbidInterface) {
-		if (null != forbidInterface) {
-			this.forbidInterface = forbidInterface;
-			mDialog.setOnKeyListener(new OnKeyListener() {
-
-				@Override
-				public boolean onKey(DialogInterface arg0, int arg1, KeyEvent arg2) {
-					if (arg2.getAction() == KeyEvent.ACTION_UP) {
-						GolukDebugUtils.e("", "------------------customDialog-------------back");
-						setData(1);
-					}
-					return true;
-				}
-			});
-			this.mDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-				@Override
-				public void onCancel(DialogInterface dialog) {
-					forbidInterface.forbidBackKey(1);
-				}
-			});
-		}
-	}
-
-
-	private void setData(int key) {
-		if (null != forbidInterface) {
-			forbidInterface.forbidBackKey(key);
-		}
-	}
-
-	public interface ForbidBack {
-		public static final int BACK_OK = 1;
-		void forbidBackKey(int backKey);
-	}
-
-
-	public void setCancel(boolean isCan) {
-		if (null != mDialog) {
-			mDialog.setCancelable(isCan);
-		}
-	}
-
-	public void setTextTitle(String title) {
-		mDialog.setMessage(title);
+    public CustomLoadingDialog(Context context, String txt) {
+        mContext = context;
+        mDialog = new ProgressDialog(context, R.style.CustomDialog);
+        if (!"".equals(txt) && txt != null) {
+            mMessage = txt;
+        } else {
+            mMessage = context.getResources().getString(R.string.str_loading_text);
+        }
+        mDialog.setMessage(mMessage);
     }
 
+    public void show() {
+        if (mDialog != null) {
+            if (!mDialog.isShowing() && isActivityRunning()) {
+                mDialog.show();
+            }
+        }
+    }
 
+    public boolean isShowing() {
+        if (null != mDialog) {
+            return mDialog.isShowing();
+        }
+        return false;
+    }
 
+    public void close() {
+        if (mDialog != null) {
+            if (mDialog.isShowing()) {
+                mDialog.dismiss();
+            }
+        }
+    }
 
+    public void setListener(final ForbidBack forbidInterface) {
+        if (null != forbidInterface) {
+            this.forbidInterface = forbidInterface;
+            mDialog.setOnKeyListener(new OnKeyListener() {
 
+                @Override
+                public boolean onKey(DialogInterface arg0, int arg1, KeyEvent arg2) {
+                    if (arg2.getAction() == KeyEvent.ACTION_UP) {
+                        GolukDebugUtils.e("", "------------------customDialog-------------back");
+                        setData(1);
+                    }
+                    return true;
+                }
+            });
+            this.mDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    forbidInterface.forbidBackKey(1);
+                }
+            });
+        }
+    }
 
+    private void setData(int key) {
+        if (null != forbidInterface) {
+            forbidInterface.forbidBackKey(key);
+        }
+    }
+
+    public interface ForbidBack {
+        public static final int BACK_OK = 1;
+
+        void forbidBackKey(int backKey);
+    }
+
+    public void setCancel(boolean isCan) {
+        if (null != mDialog) {
+            mDialog.setCancelable(isCan);
+        }
+    }
+
+    public void setTextTitle(String title) {
+        mDialog.setMessage(title);
+    }
+
+    /**
+     * Return activity is destory
+     */
+    private boolean isActivityRunning() {
+        if (mContext == null && !(mContext instanceof Activity))
+            return false;
+
+        return !((Activity) mContext).isFinishing() && !((Activity) mContext).isDestroyed();
+    }
 
 //	AlertDialog customDialog;
 //	String textTitle;
